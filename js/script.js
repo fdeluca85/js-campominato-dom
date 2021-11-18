78f043fe79df8d67a94dda32e7d5441b405dde9e
francesca <fradeluca85@gmail.com>
2021-11-18 14:12:24 +0100

    select option

--- js/script.js
+++ js/script.js
@@ -5,70 +5,197 @@
 // Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. I numeri nella lista delle bombe non possono essere duplicati.
 // In seguito l'utente clicca su ogni cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle. La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti. Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
+document.getElementById('play').addEventListener('click', function(){
+    play();
+});
 
-const container = document.querySelector(".container");
+function play() {
 
-let items= '';
+    const container = document.querySelector(".container");
 
+    const levelSelected = parseInt(document.getElementById('level').value);
+    console.log(levelSelected);
 
-    const levEasy = function () {
-        const node = document.createElement('div');
-        node.className = 'square easy';
-    
-        return node;
+    // let items= '';
+
+    switch(levelSelected){
+        case 1:
+            const levEasy = function () {
+                const node = document.createElement('div');
+                node.className = 'square easy';
+            
+                return node;
+            }
+            
+            for(let i = 1; i <= 100; i++){
+            
+                const clicked = levEasy();
+                container.appendChild(clicked);
+                clicked.innerHTML = i;
+                clicked.addEventListener('click', function() {
+                    console.log(this);
+                    this.classList.add('blue');
+                });
+            }    
+            break;
+        case 2: 
+        const levHard = function () {
+            const node = document.createElement('div');
+            node.className = 'square hard';
+        
+            return node;
+        }
+        
+        for(let i = 1; i <= 81; i++){
+        
+            const clicked = levHard();
+            container.appendChild(clicked);
+            clicked.innerHTML = i;
+            clicked.addEventListener('click', function() {
+                console.log(this);
+                this.classList.add('blue');
+            });
+        
+        }
+            break;
+        case 3: 
+        const levCrazy = function () {
+            const node = document.createElement('div');
+            node.className = 'square crazy';
+        
+            return node;
+        }
+        
+        for(let i = 1; i <= 49; i++){
+        
+            const clicked = levCrazy();
+            clicked.innerHTML = i;
+            container.appendChild(clicked);
+        
+            clicked.addEventListener('click', function() {
+                console.log(this);
+                this.classList.add('blue');
+            });
+        
+        }
     }
+
     
-    for(let i = 1; i <= 100; i++){
-    
-        const clicked = levEasy();
-        container.appendChild(clicked);
-        clicked.innerHTML = i;
-        clicked.addEventListener('click', function() {
-            console.log(this);
-            this.classList.add('blue');
-        });
-    }
+        
+        
+        
+        
+            
+        
+        
+            
 
 
 
 
-    const levHard = function () {
-        const node = document.createElement('div');
-        node.className = 'square hard';
-    
-        return node;
-    }
-    
-    for(let i = 1; i <= 81; i++){
-    
-        const clicked = levHard();
-        container.appendChild(clicked);
-        clicked.innerHTML = i;
-        clicked.addEventListener('click', function() {
-            console.log(this);
-            this.classList.add('blue');
-        });
-    
-    }
 
 
-    const levCrazy = function () {
-        const node = document.createElement('div');
-        node.className = 'square crazy';
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const container = document.querySelector(".container");
+
+// let items= '';
+
+
+//     const levEasy = function () {
+//         const node = document.createElement('div');
+//         node.className = 'square easy';
     
-        return node;
-    }
+//         return node;
+//     }
     
-    for(let i = 1; i <= 49; i++){
+//     for(let i = 1; i <= 100; i++){
     
-        const clicked = levCrazy();
-        clicked.innerHTML = i;
-        container.appendChild(clicked);
+//         const clicked = levEasy();
+//         container.appendChild(clicked);
+//         clicked.innerHTML = i;
+//         clicked.addEventListener('click', function() {
+//             console.log(this);
+//             this.classList.add('blue');
+//         });
+//     }
+
+
+
+
+//     const levHard = function () {
+//         const node = document.createElement('div');
+//         node.className = 'square hard';
     
-        clicked.addEventListener('click', function() {
-            console.log(this);
-            this.classList.add('blue');
-        });
+//         return node;
+//     }
     
-    }
+//     for(let i = 1; i <= 81; i++){
+    
+//         const clicked = levHard();
+//         container.appendChild(clicked);
+//         clicked.innerHTML = i;
+//         clicked.addEventListener('click', function() {
+//             console.log(this);
+//             this.classList.add('blue');
+//         });
+    
+//     }
+
+
+//     const levCrazy = function () {
+//         const node = document.createElement('div');
+//         node.className = 'square crazy';
+    
+//         return node;
+//     }
+    
+//     for(let i = 1; i <= 49; i++){
+    
+//         const clicked = levCrazy();
+//         clicked.innerHTML = i;
+//         container.appendChild(clicked);
+    
+//         clicked.addEventListener('click', function() {
+//             console.log(this);
+//             this.classList.add('blue');
+//         });
+    
+//     }
 
